cbe32fbf5ba1f068172815ce64a7ae69df27f014
emmanueldavids <emmanueldavids417@gmail.com>
2023-05-16 12:23:07 +0000

    Future of energy trading

--- Future_Of_Energy/Future_Of_Energy/wsgi.py
+++ Future_Of_Energy/Future_Of_Energy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for energy_transfer project.
+WSGI config for Future_Of_Energy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energy_transfer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Future_Of_Energy.settings')
 
 application = get_wsgi_application()
--- Future_Of_Energy/manage.py
+++ Future_Of_Energy/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'energy_transfer.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Future_Of_Energy.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
