f3ae84471d5ca465a4d985485f99471da286044f
Hope_Davids <hopedavids@gmail.com>
2023-05-18 11:17:57 +0000

    created the landing_page views template

--- Future_Of_Energy/energy_trading/views.py
+++ Future_Of_Energy/energy_trading/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def landing_Page(request):
+    return render(request, 'energy_trading/landing_page.html')
