77cb2184a064d7e60849dc1caab7f44d3d5bdfb6
emmanueldavids <emmanueldavids417@gmail.com>
2023-05-18 22:23:09 +0000

    modified

--- Future_Of_Energy/energy_trading/views.py
+++ Future_Of_Energy/energy_trading/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "energy_trading/index.html")
+
+def ticket(request):
+    return render(request, "energy_trading/ticket.html")
