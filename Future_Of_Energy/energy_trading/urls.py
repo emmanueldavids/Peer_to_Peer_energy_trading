31cc901835fb3dfd47d81853e9c98d7f68a1efe2
Hope_Davids <hopedavids@gmail.com>
2023-05-20 13:45:55 +0000

    added additional urls

--- Future_Of_Energy/energy_trading/urls.py
+++ Future_Of_Energy/energy_trading/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing_Page),
+    path('', views.landing_Page, name='index'),
     path('about', views.about_page, name='about'),
+    path('contact', views.contact_page, name='contact')
 ]
