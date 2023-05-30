02d87cc0f05912c81b0baf81e7b97b59c9511368
Hope_Davids <hopedavids@gmail.com>
2023-05-30 13:11:05 +0000

    added the urls of the views

--- Future_Of_Energy/trade_dashboard/urls.py
+++ Future_Of_Energy/trade_dashboard/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('alert', views.alert, name='alert'),
+    path('prices', views.prices, name='prices'),
+    path('sell', views.sell_energy, name='sell'),
+    path('feeds', views.feeds, name='feeds'),
+    path('wallet', views.wallet, name='wallet'),
+    path('transactions', views.transactions, name='transactions')
 ]
