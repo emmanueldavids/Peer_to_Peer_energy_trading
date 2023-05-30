04dbffe40403d8d9cd34df6af58ccdb939f89e7b
Hope_Davids <hopedavids@gmail.com>
2023-05-30 13:12:43 +0000

    modified the static files

--- Future_Of_Energy/account/static/js/main.js
+++
@@ -1,9 +0,0 @@
-/* YOU DONT NEED THESE, these are just for the popup you see */
-function closeTreactPopup(){ 
-    document.querySelector(".treact-popup").classList.add("hidden");
-  }
-  function openTreactPopup(){ 
-    document.querySelector(".treact-popup").classList.remove("hidden");
-  }
-  document.querySelector(".close-treact-popup").addEventListener("click", closeTreactPopup);
-  setTimeout(openTreactPopup, 3000)
