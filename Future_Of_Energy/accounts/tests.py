a019b4a8b0d1cc1860fabfddec530f79cc485722
Hope_Davids <hopedavids@gmail.com>
2023-06-01 12:38:09 +0000

    added the unittest for the app

--- Future_Of_Energy/accounts/tests.py
+++ Future_Of_Energy/accounts/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import unittest
+from django.test import TestCase, Client
 
-# Create your tests here.
+""" All unit Test Case for user Authentication """
+
+
+class checkPath(unittest.TestCase):
+    """This unit test case validates all the define paths in the app"""
+
+    def setUp(self):
+        # setup for the client
+        self.client = Client()
+
+    
+    def test_account_path(self):
+        # issuing a GET request
+        urls = [
+        '/',
+        '/accounts/register'
+        ]
+
+        for url in urls:
+            response = self.client.get(url)
+
+        # check the response status code is 200
+        self.assertEqual(response.status_code, 200)
+    
