cafc104a77eeba85147c04bdd2f3f7d7a2778353
Hope_Davids <hopedavids@gmail.com>
2023-06-01 12:39:08 +0000

    added the unittest for the urls

--- Future_Of_Energy/trade_dashboard/tests.py
+++ Future_Of_Energy/trade_dashboard/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+import unittest
+from django.test import TestCase, Client
 
-# Create your tests here.
+""" All unit Test Case for the trade Dashboard application """
+
+
+class checkPath(unittest.TestCase):
+    """This unit test case validates all the define paths in the app"""
+
+    def setUp(self):
+        # setup for the client
+        self.client = Client()
+
+    
+    def test_all_path(self):
+        # issuing a GET request
+        urls = [
+        '/',
+        '/dashboard/trade',
+        '/dashboard/alert',
+        '/dashboard/sell',
+        '/dashboard/feeds', 
+        '/dashboard/wallet', 
+        '/dashboard/transactions'
+        ]
+
+        for url in urls:
+            response = self.client.get(url)
+
+        # check the response status code is 200
+        self.assertEqual(response.status_code, 200)
+    
+
+   
