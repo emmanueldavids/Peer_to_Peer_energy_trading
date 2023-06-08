b98d35028e08b6715db8ff80ce4f26ad53437ed2
Hope_Davids <hopedavids@gmail.com>
2023-06-08 14:46:21 +0000

    registered all the objects

--- Future_Of_Energy/trade_dashboard/admin.py
+++ Future_Of_Energy/trade_dashboard/admin.py
@@ -1,3 +1,81 @@
 from django.contrib import admin
+from .models import User, Wallet, Payment, Transaction, Logs
 
-# Register your models here.
+
+"""Register all models by creating admin classes that inherit from admin.ModelAdmin """
+
+
+# @admin.register(User, Wallet, Transaction, Payment, Logs)
+# class MyModelAdmin(admin.ModelAdmin):
+#     pass
+
+
+
+@admin.register(User)
+class UserDashboard(admin.ModelAdmin):
+    """ This class registers the User model to the Admin interface"""
+
+    list_display = [
+        'id', 
+        'username', 
+        'email', 
+        'is_staff', 
+        'is_superuser'
+        ]
+
+
+@admin.register(Wallet)
+class UserWallet(admin.ModelAdmin):
+    """ This class registers the Wallet Model to the Admin interface"""
+
+    list_display = [
+        'user_id', 
+        'wallet_id',
+        'balance', 
+        'previous_balance',
+        'currency', 
+        'created_at', 
+        'updated_at', 
+        'status'
+        ]
+    
+
+@admin.register(Payment)
+class UserPayment(admin.ModelAdmin):
+    """ This class registers the payment Model to the admin interface """
+
+    list_display = [
+        'payment_id',
+        'wallet_id',
+        'transaction_id',
+        'timestamp'
+    ]
+
+
+
+@admin.register(Transaction)
+class PaymentTransaction(admin.ModelAdmin):
+    """ This class registers the Transaction object to the admin interface """
+
+    list_display = [
+        'transaction_id',
+        'wallet_id',
+        'capacity',
+        'amount',
+        'start_time',
+        'end_time',
+    ]
+
+
+@admin.register(Logs)
+class All_Logs(admin.ModelAdmin):
+    """ This class registers the Logs object to the admin interface """
+
+    list_display = [
+        'log_id',
+        'transaction_id',
+        'payment_id',
+        'wallet_id',
+        'user_id',
+        'timestamp'
+    ]
